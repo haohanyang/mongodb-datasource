afd5a5218b0e24d442106308bc490bd7e4fcac79
Haohan Yang <yanghaohanmail@gmail.com>
2024-09-15 22:08:51 +0000

    fix config editor tests

--- tests/configEditor.spec.ts
+++ tests/configEditor.spec.ts
@@ -1,24 +1,35 @@
 import { test, expect } from '@grafana/plugin-e2e';
 import { MongoDataSourceOptions, MySecureJsonData } from '../src/types';
 
-test('"Save & test" should be successful when configuration is valid', async ({
+test('"Save & test" should be successful when mongo without auth config is valid', async ({
   createDataSourceConfigPage,
   readProvisionedDataSource,
   page,
 }) => {
-  const ds = await readProvisionedDataSource<MongoDataSourceOptions, MySecureJsonData>({ fileName: 'datasources.yml' });
+  const ds = await readProvisionedDataSource<MongoDataSourceOptions, MySecureJsonData>({ fileName: 'test/mongo-no-auth.yml' });
+  console.log(ds.jsonData)
   const configPage = await createDataSourceConfigPage({ type: ds.type });
-  await page.getByRole('textbox', { name: 'Password' }).fill(ds.secureJsonData?.password ?? '');
+  await page.getByLabel("Host").fill(ds.jsonData.host || "")
+  await page.getByLabel("Port").fill(ds.jsonData.port?.toString() || "")
+  await page.getByLabel("Database").fill(ds.jsonData.database || "")
+  await page.getByRole("radio", { name: "None" }).check()
   await expect(configPage.saveAndTest()).toBeOK();
 });
 
-test('"Save & test" should fail when configuration is invalid', async ({
+test('"Save & test" should be successful when mongo username-password auth config is valid', async ({
   createDataSourceConfigPage,
   readProvisionedDataSource,
   page,
 }) => {
-  const ds = await readProvisionedDataSource<MongoDataSourceOptions, MySecureJsonData>({ fileName: 'datasources.yml' });
+  const ds = await readProvisionedDataSource<MongoDataSourceOptions, MySecureJsonData>({ fileName: 'test/mongo-username-password-auth.yml' });
+  console.log(ds.jsonData)
+  console.log(ds.secureJsonData?.password)
   const configPage = await createDataSourceConfigPage({ type: ds.type });
-  await expect(configPage.saveAndTest()).not.toBeOK();
-  await expect(configPage).toHaveAlert('error', { hasText: 'API key is missing' });
+  await page.getByLabel("Host").fill(ds.jsonData.host || "")
+  await page.getByLabel("Port").fill(ds.jsonData.port?.toString() || "")
+  await page.getByLabel("Database").fill(ds.jsonData.database || "")
+  await page.getByRole("radio", { name: "Username/Password", exact: true }).check()
+  await page.getByLabel("Username", { exact: true }).fill(ds.jsonData.username || "")
+  await page.getByLabel("Password", { exact: true }).fill(ds.secureJsonData?.password || "")
+  await expect(configPage.saveAndTest()).toBeOK();
 });
