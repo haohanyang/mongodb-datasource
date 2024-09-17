75eb82e21975d0cf5c1580e2d9fb46a445689a57
Haohan Yang <yanghaohanmail@gmail.com>
2024-09-17 21:42:56 +0200

    add experimental "getTableFramesFromQuery"

--- pkg/plugin/query.go
+++ pkg/plugin/query.go
@@ -3,6 +3,7 @@ package plugin
 import (
 	"context"
 	"errors"
+	"fmt"
 	"time"
 
 	"github.com/grafana/grafana-plugin-sdk-go/data"
@@ -64,4 +65,156 @@ func updateFrameData[T any](cursor *mongo.Cursor, allFrames map[string]*data.Fra
 			data.NewField("Value", nil, []T{tr.Value}))
 	}
 	return nil
+}
+
+func getTableFramesFromQuery(ctx context.Context, cursor *mongo.Cursor) (*data.Frame, error) {
+	frame := data.NewFrame("Table")
+
+	columns := make(map[string]columnDefinition)
+	rowCount := 0
+	for cursor.Next(ctx) {
+		var result bson.Raw
+		if err := cursor.Decode(&result); err != nil {
+			return nil, err
+		}
+
+		elements, err := result.Elements()
+		if err != nil {
+			return nil, err
+		}
+
+		for _, element := range elements {
+			if rowCount == 0 {
+				cd := initFrameField(element, frame)
+				columns[cd.name] = cd
+			} else {
+				name := element.Key()
+				if c, ok := columns[name]; ok {
+					err = c.addField(frame, element.Value())
+					if err != nil {
+						return nil, err
+					}
+				} else {
+					return nil, fmt.Errorf("extra field \"%s\"", name)
+				}
+			}
+		}
+	}
+
+	return frame, nil
+}
+
+func initFrameField(element bson.RawElement, frame *data.Frame) columnDefinition {
+	key := element.Key()
+	value := element.Value()
+	var field *data.Field
+
+	d := columnDefinition{
+		name:      key,
+		valueType: value.Type,
+	}
+
+	switch value.Type {
+	case bson.TypeBoolean:
+		field = data.NewField(key, nil, []bool{value.Boolean()})
+		frame.Fields = append(frame.Fields, field)
+
+		index := len(frame.Fields) - 1
+		d.addField = func(f *data.Frame, rv bson.RawValue) error {
+			if rv.Type != bson.TypeBoolean {
+				return fmt.Errorf("field \"%s\" should have boolean type", key)
+			}
+			f.Fields[index].Append(rv.Boolean())
+			return nil
+		}
+	case bson.TypeInt32:
+		field = data.NewField(key, nil, []int32{value.Int32()})
+		frame.Fields = append(frame.Fields, field)
+
+		index := len(frame.Fields) - 1
+		d.addField = func(f *data.Frame, rv bson.RawValue) error {
+			if rv.Type != bson.TypeInt32 {
+				return fmt.Errorf("field \"%s\" should have int32 type", key)
+			}
+			f.Fields[index].Append(rv.Int32())
+			return nil
+		}
+
+	case bson.TypeInt64:
+		field = data.NewField(key, nil, []int64{value.Int64()})
+		frame.Fields = append(frame.Fields, field)
+
+		index := len(frame.Fields) - 1
+		d.addField = func(f *data.Frame, rv bson.RawValue) error {
+			if rv.Type != bson.TypeInt64 {
+				return fmt.Errorf("field \"%s\" should have int64 type", key)
+			}
+			f.Fields[index].Append(rv.Int64())
+			return nil
+		}
+
+	case bson.TypeDouble:
+		field = data.NewField(key, nil, []float64{value.Double()})
+		frame.Fields = append(frame.Fields, field)
+
+		index := len(frame.Fields) - 1
+		d.addField = func(f *data.Frame, rv bson.RawValue) error {
+			if rv.Type != bson.TypeDouble {
+				return fmt.Errorf("field \"%s\" should have double type", key)
+			}
+			f.Fields[index].Append(rv.Double())
+			return nil
+		}
+
+	case bson.TypeString:
+		field = data.NewField(key, nil, []string{value.StringValue()})
+		frame.Fields = append(frame.Fields, field)
+
+		index := len(frame.Fields) - 1
+		d.addField = func(f *data.Frame, rv bson.RawValue) error {
+			if rv.Type != bson.TypeString {
+				return fmt.Errorf("field \"%s\" should have string type", key)
+			}
+			f.Fields[index].Append(rv.StringValue())
+			return nil
+		}
+
+	case bson.TypeDateTime:
+		field = data.NewField(key, nil, []time.Time{value.Time()})
+		frame.Fields = append(frame.Fields, field)
+
+		index := len(frame.Fields) - 1
+		d.addField = func(f *data.Frame, rv bson.RawValue) error {
+			if rv.Type != bson.TypeDateTime {
+				return fmt.Errorf("field \"%s\" should have datetime type", key)
+			}
+			f.Fields[index].Append(rv.Time())
+			return nil
+		}
+
+	case bson.TypeObjectID:
+		field = data.NewField(key, nil, []string{value.ObjectID().Hex()})
+		frame.Fields = append(frame.Fields, field)
+
+		index := len(frame.Fields) - 1
+		d.addField = func(f *data.Frame, rv bson.RawValue) error {
+			if rv.Type != bson.TypeObjectID {
+				return fmt.Errorf("field \"%s\" should have objectId type", key)
+			}
+			f.Fields[index].Append(rv.ObjectID().Hex())
+			return nil
+		}
+
+	default:
+		field = data.NewField(key, nil, []string{value.String()})
+		frame.Fields = append(frame.Fields, field)
+
+		index := len(frame.Fields) - 1
+		d.addField = func(f *data.Frame, rv bson.RawValue) error {
+			f.Fields[index].Append(rv.String())
+			return nil
+		}
+	}
+
+	return d
 }
--- pkg/plugin/types.go
+++ pkg/plugin/types.go
@@ -2,6 +2,10 @@ package plugin
 
 import (
 	"time"
+
+	"github.com/grafana/grafana-plugin-sdk-go/data"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/bsontype"
 )
 
 type queryModel struct {
@@ -14,4 +18,10 @@ type timeSeriesRow[T any] struct {
 	Timestamp time.Time `bson:"ts"`
 	Name      string    `bson:"name"`
 	Value     T         `bson:"value"`
+}
+
+type columnDefinition struct {
+	name      string
+	valueType bsontype.Type
+	addField  func(*data.Frame, bson.RawValue) error
 }
