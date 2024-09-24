929893963c69575a27a307430c07e8f4d69d94ac
Haohan Yang <yanghaohanmail@gmail.com>
2024-09-24 21:37:53 +0200

    Add js query mode

--- src/components/QueryEditor.tsx
+++ src/components/QueryEditor.tsx
@@ -1,9 +1,10 @@
 import React, { ChangeEvent, useRef, useState } from 'react';
-import { Button, CodeEditor, Divider, Field, InlineField, InlineFieldRow, Input, Select } from '@grafana/ui';
+import { Button, CodeEditor, Divider, Field, InlineField, InlineFieldRow, InlineSwitch, Input, Select } from '@grafana/ui';
 import { QueryEditorProps, SelectableValue } from '@grafana/data';
 import { DataSource } from '../datasource';
 import { MongoDataSourceOptions, MongoQuery, QueryType } from '../types';
 import * as monacoType from 'monaco-editor/esm/vs/editor/editor.api';
+
 
 type Props = QueryEditorProps<DataSource, MongoQuery, MongoDataSourceOptions>;
 
@@ -22,6 +23,9 @@ export function QueryEditor({ query, onChange }: Props) {
 
   const codeEditorRef = useRef<monacoType.editor.IStandaloneCodeEditor | null>(null);
   const [queryTextError, setQueryTextError] = useState<string | null>(null);
+  const [jsQueryMode, setJsQueryMode] = useState<boolean>(false)
+  const [jsQueryText, setJsQueryText] = useState<string>("")
+  const [jsQueryError, setJsQueryError] = useState<string | null>(null)
 
   const onQueryTextChange = (queryText: string) => {
     onChange({ ...query, queryText: queryText });
@@ -42,6 +46,40 @@ export function QueryEditor({ query, onChange }: Props) {
     }
   };
 
+  const onJsQueryTextChange = (jsQueryText: string) => {
+    setJsQueryText(jsQueryText)
+    const regex = /db\.(.+)\.aggregate\((.+)\)$/
+    const match = jsQueryText.trim().replace(/(;$)/g, "").replace(/(\r\n|\n|\r)/gm, "")
+      .match(regex);
+
+    if (match) {
+      const collection = match[1];
+      const queryText = match[2];
+      onChange({ ...query, queryText: queryText, collection: collection })
+
+      if (!queryText) {
+        setQueryTextError('Please enter the query');
+      } else {
+        try {
+          const queryJson = JSON.parse(queryText);
+          if (!Array.isArray(queryJson)) {
+            setJsQueryError("Invalid query");
+          } else {
+            setJsQueryError(null);
+          }
+        } catch (e) {
+          setJsQueryError("Invalid query");
+        }
+      }
+    } else {
+      setJsQueryError("Invalid query")
+    }
+  };
+
+  const onToggleJSQueryMode = (e: React.FormEvent<HTMLInputElement>) => {
+    setJsQueryMode(e.currentTarget.checked)
+  }
+
   const onQueryTypeChange = (sv: SelectableValue<string>) => {
     onChange({ ...query, queryType: sv.value });
   };
@@ -60,25 +98,31 @@ export function QueryEditor({ query, onChange }: Props) {
     }
   };
 
-  const { queryText, collection, queryType } = query;
-
   return (
     <>
       <InlineFieldRow>
         <InlineField label="Query Type">
-          <Select id="query-editor-query-type" options={queryTypes} value={queryType || QueryType.TIMESERIES} onChange={onQueryTypeChange}></Select>
+          <Select id="query-editor-query-type" options={queryTypes} value={query.queryType || QueryType.TIMESERIES} onChange={onQueryTypeChange}></Select>
         </InlineField>
-        <InlineField label="Collection" tooltip="Enter the collection to query"
-          error="Please enter the collection" invalid={!collection}>
-          <Input id="query-editor-collection" onChange={onCollectionChange} value={collection} required />
-        </InlineField>
+        {!jsQueryMode && <InlineField label="Collection" tooltip="Enter the collection to query"
+          error="Please enter the collection" invalid={!query.collection}>
+          <Input id="query-editor-collection" onChange={onCollectionChange} value={query.collection} required />
+        </InlineField>}
       </InlineFieldRow>
       <Divider />
-      <Field label="Query Text" description="Enter the Mongo Aggregation Pipeline"
-        error={queryTextError} invalid={queryTextError != null}>
-        <CodeEditor onEditorDidMount={onCodeEditorDidMount} width="100%" height={300} language="json"
-          onBlur={onQueryTextChange} value={queryText || ""} showMiniMap={false} />
-      </Field>
+      <InlineField label="Use JavaScript Query">
+        <InlineSwitch id="query-editor-use-js-query" value={jsQueryMode} onChange={onToggleJSQueryMode} />
+      </InlineField>
+      {jsQueryMode ?
+        <Field label="Query Text" description="Enter the Mongo Aggregation Pipeline (JavaScript)"
+          error={jsQueryError} invalid={jsQueryError != null}>
+          <CodeEditor onEditorDidMount={onCodeEditorDidMount} width="100%" height={300} language="javascript"
+            onBlur={onJsQueryTextChange} value={jsQueryText || ""} showMiniMap={false} />
+        </Field> : <Field label="Query Text" description="Enter the Mongo Aggregation Pipeline (JSON)"
+          error={queryTextError} invalid={queryTextError != null}>
+          <CodeEditor onEditorDidMount={onCodeEditorDidMount} width="100%" height={300} language="json"
+            onBlur={onQueryTextChange} value={query.queryText || ""} showMiniMap={false} />
+        </Field>}
       <Button onClick={onFormatQueryText}>Format</Button>
     </>
   );
--- src/datasource.ts
+++ src/datasource.ts
@@ -1,7 +1,8 @@
-import { DataSourceInstanceSettings, CoreApp, ScopedVars } from '@grafana/data';
+import { DataSourceInstanceSettings, CoreApp, ScopedVars, DataQueryRequest, DataQueryResponse, DateTime } from '@grafana/data';
 import { DataSourceWithBackend, getTemplateSrv } from '@grafana/runtime';
 
 import { MongoQuery, MongoDataSourceOptions, DEFAULT_QUERY } from './types';
+import { Observable } from 'rxjs';
 
 function isJsonStringValid(jsonString: string) {
   try {
@@ -13,7 +14,14 @@ function isJsonStringValid(jsonString: string) {
   } catch (e) {
     return false;
   }
-  return true;
+}
+
+function datetimeToJson(datetime: DateTime) {
+  return JSON.stringify({
+    $date: {
+      $numberLong: datetime.toDate().getTime().toString()
+    }
+  });
 }
 
 export class DataSource extends DataSourceWithBackend<MongoQuery, MongoDataSourceOptions> {
@@ -34,5 +42,16 @@ export class DataSource extends DataSourceWithBackend<MongoQuery, MongoDataSourc
 
   filterQuery(query: MongoQuery): boolean {
     return !!query.queryText && !!query.collection && isJsonStringValid(query.queryText!);
+  }
+
+  query(request: DataQueryRequest<MongoQuery>): Observable<DataQueryResponse> {
+    const queries = request.targets.map(query => {
+      return {
+        ...query,
+        queryText: query.queryText?.replaceAll(/"\$from"/g, datetimeToJson(request.range.from))
+          .replaceAll(/"\$to"/g, datetimeToJson(request.range.to))
+      };
+    });
+    return super.query({ ...request, targets: queries });
   }
 }
