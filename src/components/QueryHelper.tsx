2831e23d5acc0b866d47882ace616a3e6d571dbe
Nobody <17411508+Nobodyno-boop@users.noreply.github.com>
2024-10-12 14:46:05 +0200

    (Optional) add definition of type of query in the query helper

--- src/components/QueryHelper.tsx
+++ src/components/QueryHelper.tsx
@@ -1,6 +1,36 @@
 import React from "react";
 import { QueryEditorHelpProps } from "@grafana/data";
+import {Divider, Text} from "@grafana/ui";
 
 export function QueryHelper(_props: QueryEditorHelpProps) {
-    return <p>TODO</p>;
-};
+  return (
+    <div>
+      <Text element="h1" color="primary">
+        Query Language Types and Differences
+      </Text>
+
+      <Divider></Divider>
+      <div>
+        <p>
+          <Text element="span" weight="bold" color="info">
+            JSON
+          </Text>
+          : JSON aggregate of mongodb query.
+        </p>
+        <p>
+          <Text element="span" weight="bold" color="info">
+            JavaScript
+          </Text>
+          :JavaScript aggregate of mongodb query for the legacy support
+        </p>
+        <p>
+          <Text element="span" weight="bold" color="info">
+            JavaScript Shadow
+          </Text>
+          : JavaScript function that return aggregate of mongodb query with evaluation
+          support.
+        </p>
+      </div>
+    </div>
+  );
+}
