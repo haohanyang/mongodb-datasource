15c756dc5616d1ad458e1474650ebe6979e074e5
Haohan Yang <37651510+haohanyang@users.noreply.github.com>
2024-10-22 19:37:40 +0200

    Add query variable support (#17)
    * Add query variable support
    
    * add git hash info
    
    * Revert "add git hash info"
    
    This reverts commit 630f29c672f0fb7acd490b4f4afc860a4e559fdc.
    
    * update version
    
    * preview values in text
    
    * test update plugin.json script

--- src/module.ts
+++ src/module.ts
@@ -3,9 +3,11 @@ import { DataSource } from "./datasource";
 import { ConfigEditor } from "./components/ConfigEditor";
 import { QueryEditor } from "./components/QueryEditor";
 import { QueryHelper } from "./components/QueryHelper";
+import { VariableQueryEditor } from "./components/VariableQueryEditor";
 import { MongoQuery, MongoDataSourceOptions } from "./types";
 
 export const plugin = new DataSourcePlugin<DataSource, MongoQuery, MongoDataSourceOptions>(DataSource)
   .setConfigEditor(ConfigEditor)
   .setQueryEditor(QueryEditor)
-  .setQueryEditorHelp(QueryHelper);
+  .setQueryEditorHelp(QueryHelper)
+  .setVariableQueryEditor(VariableQueryEditor);
