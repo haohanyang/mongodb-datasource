19307edc00e9608951359756ceeedfb4065f0dbe
Haohan Yang <37651510+haohanyang@users.noreply.github.com>
2024-12-13 23:54:14 +0100

    Change test ts files to js (#34)

--- tests/configEditor.spec.js
+++ tests/configEditor.spec.js
@@ -1,12 +1,11 @@
 import { test, expect } from "@grafana/plugin-e2e";
-import { MongoDataSourceOptions, MySecureJsonData } from "../src/types";
 
 test("\"Save & test\" should be successful when mongo without auth config is valid", async ({
     createDataSourceConfigPage,
     readProvisionedDataSource,
     page,
 }) => {
-    const ds = await readProvisionedDataSource<MongoDataSourceOptions, MySecureJsonData>({ fileName: "test/mongo-no-auth.yml" });
+    const ds = await readProvisionedDataSource({ fileName: "test/mongo-no-auth.yml" });
     const configPage = await createDataSourceConfigPage({ type: ds.type });
     await page.getByLabel("Host").fill(ds.jsonData.host || "");
     await page.getByLabel("Port").fill(ds.jsonData.port?.toString() || "");
@@ -20,7 +19,7 @@ test("\"Save & test\" should be successful when mongo username-password auth con
     readProvisionedDataSource,
     page,
 }) => {
-    const ds = await readProvisionedDataSource<MongoDataSourceOptions, MySecureJsonData>({ fileName: "test/mongo-username-password-auth.yml" });
+    const ds = await readProvisionedDataSource({ fileName: "test/mongo-username-password-auth.yml" });
     const configPage = await createDataSourceConfigPage({ type: ds.type });
     await page.getByLabel("Host").fill(ds.jsonData.host || "");
     await page.getByLabel("Port").fill(ds.jsonData.port?.toString() || "");
