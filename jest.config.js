bf4a89dde8250680de9c770add29ed3d91b55b8d
Haohan Yang <yanghaohanmail@gmail.com>
2025-01-26 16:16:11 +0100

    Move e2e test files

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,10 @@
-// force timezone to UTC to allow tests to work regardless of local timezone
-// generally used by snapshots, but can affect specific tests
 process.env.TZ = 'UTC';
 
+/**
+ * @type {import('jest').Config}
+ */
 module.exports = {
   // Jest configuration provided by Grafana scaffolding
   ...require('./.config/jest.config'),
+  testMatch: ['<rootDir>/tests/*.{js,jsx}'],
 };
