3b9d61e7d50774ff2c253fcf6b96fab63909aaf4
Haohan Yang <yanghaohanmail@gmail.com>
2025-09-17 15:54:17 +0000

    Add tls auth config test

--- tests/configEditor.spec.js
+++ tests/configEditor.spec.js
@@ -1,31 +1,48 @@
-import { test, expect } from "@grafana/plugin-e2e";
+import { test, expect } from '@grafana/plugin-e2e';
 
-test("\"Save & test\" should be successful when mongo without auth config is valid", async ({
-    createDataSourceConfigPage,
-    readProvisionedDataSource,
-    page,
+test('"Save & test" should be successful when mongo without auth config is valid', async ({
+  createDataSourceConfigPage,
+  readProvisionedDataSource,
+  page,
 }) => {
-    const ds = await readProvisionedDataSource({ fileName: "test/mongo-no-auth.yml" });
-    const configPage = await createDataSourceConfigPage({ type: ds.type });
-    await page.getByLabel("Host").fill(ds.jsonData.host || "");
-    await page.getByLabel("Port").fill(ds.jsonData.port?.toString() || "");
-    await page.getByLabel("Database").fill(ds.jsonData.database || "");
-    await page.getByRole("radio", { name: "None" }).check();
-    await expect(configPage.saveAndTest()).toBeOK();
+  const ds = await readProvisionedDataSource({ fileName: 'test/mongo-no-auth.yml' });
+  const configPage = await createDataSourceConfigPage({ type: ds.type });
+  await page.getByLabel('Host').fill(ds.jsonData.host ?? '');
+  await page.getByLabel('Port').fill(ds.jsonData.port?.toString() ?? '');
+  await page.getByLabel('Database').fill(ds.jsonData.database ?? '');
+  await page.getByRole('radio', { name: 'None' }).check();
+  await expect(configPage.saveAndTest()).toBeOK();
 });
 
-test("\"Save & test\" should be successful when mongo username-password auth config is valid", async ({
-    createDataSourceConfigPage,
-    readProvisionedDataSource,
-    page,
+test('"Save & test" should be successful when mongo username-password auth config is valid', async ({
+  createDataSourceConfigPage,
+  readProvisionedDataSource,
+  page,
 }) => {
-    const ds = await readProvisionedDataSource({ fileName: "test/mongo-username-password-auth.yml" });
-    const configPage = await createDataSourceConfigPage({ type: ds.type });
-    await page.getByLabel("Host").fill(ds.jsonData.host || "");
-    await page.getByLabel("Port").fill(ds.jsonData.port?.toString() || "");
-    await page.getByLabel("Database").fill(ds.jsonData.database || "");
-    await page.getByRole("radio", { name: "Username/Password", exact: true }).check();
-    await page.getByLabel("Username", { exact: true }).fill(ds.jsonData.username || "");
-    await page.getByLabel("Password", { exact: true }).fill(ds.secureJsonData?.password || "");
-    await expect(configPage.saveAndTest()).toBeOK();
+  const ds = await readProvisionedDataSource({ fileName: 'test/mongo-username-password-auth.yml' });
+  const configPage = await createDataSourceConfigPage({ type: ds.type });
+  await page.getByLabel('Host').fill(ds.jsonData.host ?? '');
+  await page.getByLabel('Port').fill(ds.jsonData.port?.toString() ?? '');
+  await page.getByLabel('Database').fill(ds.jsonData.database ?? '');
+  await page.getByRole('radio', { name: 'Username/Password', exact: true }).check();
+  await page.getByLabel('Username', { exact: true }).fill(ds.jsonData.username ?? '');
+  await page.getByLabel('Password', { exact: true }).fill(ds.secureJsonData?.password ?? '');
+  await expect(configPage.saveAndTest()).toBeOK();
+});
+
+test('"Save & test" should be successful when mongo tls auth config is valid', async ({
+  createDataSourceConfigPage,
+  readProvisionedDataSource,
+  page,
+}) => {
+  const ds = await readProvisionedDataSource({ fileName: 'test/mongo-tls-auth.yml' });
+  const configPage = await createDataSourceConfigPage({ type: ds.type });
+  await page.getByLabel('Host').fill(ds.jsonData.host ?? '');
+  await page.getByLabel('Port').fill(ds.jsonData.port?.toString() ?? '');
+  await page.getByLabel('Database').fill(ds.jsonData.database ?? '');
+  await page.getByRole('radio', { name: 'TLS/SSL', exact: true }).check();
+  await page.getByLabel('Certificate Authority', { exact: true }).fill(ds.jsonData.caCertPath ?? '');
+  await page.getByLabel('Client Certificate', { exact: true }).fill(ds.jsonData.clientCertPath ?? '');
+  await page.getByLabel('Client Key', { exact: true }).fill(ds.jsonData.clientKeyPath ?? '');
+  await expect(configPage.saveAndTest()).toBeOK();
 });
