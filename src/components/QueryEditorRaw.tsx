f98a763ce99a92aa9b8c782556ee6b7a276f2bea
Haohan Yang <yanghaohanmail@gmail.com>
2025-10-28 07:35:02 +0000

    Add custom theme

--- src/components/QueryEditorRaw.tsx
+++ src/components/QueryEditorRaw.tsx
@@ -1,5 +1,5 @@
 import React, { useRef, useCallback } from 'react';
-import { CodeEditor, type MonacoEditor } from '@grafana/ui';
+import { CodeEditor, type MonacoEditor, useTheme2 } from '@grafana/ui';
 import { useAutocomplete } from '../editor/autocomplete';
 import { useValidation } from '../editor/validation';
 import { useHover } from '../editor/hover';
@@ -17,6 +17,8 @@ interface QueryEditorRawProps {
 }
 
 export function QueryEditorRaw({ query, onBlur, language, width, height, fontSize, children }: QueryEditorRawProps) {
+  const theme = useTheme2();
+
   const monacoRef = useRef<MonacoEditor | null>(null);
 
   const setupAutocompleteFn = useAutocomplete();
@@ -34,6 +36,14 @@ export function QueryEditorRaw({ query, onBlur, language, width, height, fontSiz
   return (
     <div style={{ width }}>
       <CodeEditor
+        onBeforeEditorMount={(monaco) => {
+          monaco.editor.defineTheme('code-editor-theme', {
+            base: theme.isDark ? 'vs-dark' : 'vs',
+            inherit: true,
+            rules: [{ token: 'identifier.js', foreground: theme.isDark ? '#00c04b' : '#00ab41' }],
+            colors: {},
+          });
+        }}
         onEditorDidMount={(editor, monaco) => {
           monacoRef.current = editor;
           setupValidationFn(editor, monaco);
@@ -55,7 +65,7 @@ export function QueryEditorRaw({ query, onBlur, language, width, height, fontSiz
         value={query}
         showMiniMap={false}
         showLineNumbers={true}
-        monacoOptions={fontSize ? { fontSize: fontSize, codeLens: true } : undefined}
+        monacoOptions={fontSize ? { fontSize: fontSize, codeLens: true, theme: 'code-editor-theme' } : undefined}
       />
       {children && children({ formatQuery })}
     </div>
