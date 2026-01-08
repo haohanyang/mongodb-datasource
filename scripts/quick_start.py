9a15ba01c3e3b23d98e4324dccaf1092100851f3
Haohan Yang <yanghaohanmail@gmail.com>
2026-01-08 23:50:33 +0100

    [skip ci] Add option to run the latest pre-release in quick_start.py

--- scripts/quick_start.py
+++ scripts/quick_start.py
@@ -6,35 +6,47 @@ import os
 import glob
 import subprocess
 import shutil
-from sys import platform
+import sys
 
 dist_dir = "mongodb-datasource"
 if os.path.isdir(dist_dir):
     shutil.rmtree(dist_dir)
 
-# Download the latest release build
-with urllib.request.urlopen(
-    "https://api.github.com/repos/haohanyang/mongodb-datasource/releases/latest"
-) as response:
-    data = json.loads(response.read())
+pre_release = False
 
-    for asset in data["assets"]:
-        if asset["content_type"] == "application/zip":
-            with tempfile.NamedTemporaryFile() as temp_file:
-                print("Downloading " + asset["name"] + "...")
-                urllib.request.urlretrieve(
-                    asset["browser_download_url"], temp_file.name
-                )
-                print("Extracting files to " + dist_dir)
-                with zipfile.ZipFile(temp_file.name, "r") as zip_ref:
-                    zip_ref.extractall(os.getcwd())
-    os.rename("haohanyang-mongodb-datasource", "mongodb-datasource")
+if "--pre-release" in sys.argv:
+    pre_release = True
+
+if pre_release:
+    # Download the latest release (can be pre-release) build
+    with urllib.request.urlopen(
+        "https://api.github.com/repos/haohanyang/mongodb-datasource/releases"
+    ) as response:
+        data = json.loads(response.read())[0]
+else:
+    # Download the latest release build
+    with urllib.request.urlopen(
+        "https://api.github.com/repos/haohanyang/mongodb-datasource/releases/latest"
+    ) as response:
+        data = json.loads(response.read())
+
+for asset in data["assets"]:
+    if asset["content_type"] == "application/zip":
+        with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+            print("Downloading " + asset["name"] + "...")
+            urllib.request.urlretrieve(
+                asset["browser_download_url"], temp_file.name
+            )
+            print("Extracting files to " + dist_dir)
+            with zipfile.ZipFile(temp_file.name, "r") as zip_ref:
+                zip_ref.extractall(os.getcwd())
+os.rename("haohanyang-mongodb-datasource", "mongodb-datasource")
 
 # Grant execute permission go binaries
 for bin in glob.glob("mongodb-datasource/gpx_mongodb_datasource_*"):
     os.chmod(bin, os.stat(bin).st_mode | 755)
 
-if platform == "linux" or platform == "linux2":
+if sys.platform == "linux" or sys.platform == "linux2":
     # "sudo" only for linux
     subprocess.call(
         ["sudo", "docker", "compose", "-f", "docker-compose.prod.yaml", "up", "-d"]
