0a941403ed48da3923e00167328031e70665fc85
Haohan Yang <yanghaohanmail@gmail.com>
2026-01-25 19:50:32 +0100

    Add interpolation

--- src/variables.ts
+++ src/variables.ts
@@ -1,5 +1,6 @@
 import { CustomVariableSupport, DataQueryRequest, DataQueryResponse } from '@grafana/data';
 import { MongoDBDataSource } from 'datasource';
+import { getTemplateSrv } from '@grafana/runtime';
 import { from, map, Observable } from 'rxjs';
 import { VariableQueryEditor } from './components/VariableQueryEditor';
 import { MongoDBVariableQuery } from 'types';
@@ -12,9 +13,12 @@ export class MongoDBVariableSupport extends CustomVariableSupport<MongoDBDataSou
 
   query(request: DataQueryRequest<MongoDBVariableQuery>): Observable<DataQueryResponse> {
     const [target] = request.targets;
+
+    const interpolated = getTemplateSrv().replace(target.queryText);
+
     const result = this.datasource.metricFindQuery({
       refId: target.refId,
-      queryText: target.queryText,
+      queryText: interpolated,
       collection: target.collection,
     });
     return from(result).pipe(map((data) => ({ data })));
