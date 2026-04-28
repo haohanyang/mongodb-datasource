36bde389f94758052f208c102505980f822d0ec9
Chahat Upadhyay <112254165+ChahatUpadhyay@users.noreply.github.com>
2026-04-28 16:18:45 +0530

    Corrected Code

--- src/variables.ts
+++ src/variables.ts
@@ -1,52 +1,54 @@
-import { CustomVariableSupport, DataQueryRequest, DataQueryResponse, FieldType } from '@grafana/data';
+import {
+  CustomVariableSupport,
+  DataQueryRequest,
+  DataQueryResponse,
+  FieldType,
+  MutableDataFrame,
+} from '@grafana/data';
 import { MongoDBDataSource } from 'datasource';
 import { getTemplateSrv } from '@grafana/runtime';
-import { from, map, Observable } from 'rxjs';
 import { VariableQueryEditor } from './components/VariableQueryEditor';
 import { MongoDBVariableQuery } from 'types';
- 
+
 // Query variable support
 export class MongoDBVariableSupport extends CustomVariableSupport<MongoDBDataSource> {
   constructor(private readonly datasource: MongoDBDataSource) {
     super();
   }
- 
-  query(request: DataQueryRequest<MongoDBVariableQuery>): Observable<DataQueryResponse> {
+
+  query(request: DataQueryRequest<MongoDBVariableQuery>): Promise<DataQueryResponse> {
     const [target] = request.targets;
- 
+
     const interpolated = getTemplateSrv().replace(target.queryText);
- 
-    const result = this.datasource.metricFindQuery({
+
+    return this.datasource.metricFindQuery({
       refId: target.refId,
       queryText: interpolated,
       collection: target.collection,
-    });
- 
-    return from(result).pipe(
-      map((metricFindValues) => ({
-        data: [
+    }).then((metricFindValues) => {
+      const frame = new MutableDataFrame({
+        name: 'variable_query_result',
+        fields: [
           {
-            name: 'variable_query_result',
-            fields: [
-              {
-                name: '__text',
-                type: FieldType.string,
-                values: metricFindValues.map((v) => v.text),
-                config: {},
-              },
-              {
-                name: '__value',
-                type: FieldType.string,
-                values: metricFindValues.map((v) => String(v.value ?? v.text)),
-                config: {},
-              },
-            ],
-            length: metricFindValues.length,
+            name: '__text',
+            type: FieldType.string,
+            values: metricFindValues.map((v) => v.text),
+          },
+          {
+            name: '__value',
+            type: FieldType.string,
+            values: metricFindValues.map((v) =>
+              v.value !== undefined ? String(v.value) : String(v.text)
+            ),
           },
         ],
-      }))
-    );
+      });
+
+      return {
+        data: [frame],
+      };
+    });
   }
- 
+
   editor = VariableQueryEditor;
 }
